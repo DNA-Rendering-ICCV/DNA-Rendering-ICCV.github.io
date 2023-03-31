6be262a9f1e8060fca4967c8aa02033a369d9d75
DNA-Rendering-ICCV <127970233+DNA-Rendering-ICCV@users.noreply.github.com>
2023-03-31 10:48:53 +0800

    Add files via upload

--- assets/video/home/generate_mp4.py
+++ assets/video/home/generate_mp4.py
@@ -3,4 +3,4 @@ src_dir = "src"
 videos = os.listdir(src_dir)
 for video in videos:
 
-    os.system(f"ffmpeg -y -i {src_dir}/{video} -vf scale=w=0.5*iw:h=0.5*ih {video.replace('.mp4','.mp4')}")
+    os.system(f"ffmpeg -y -i {src_dir}/{video} -vf scale=w=0.5*iw:h=0.5*ih -crf 30 {video.replace('.mp4','.mp4')}")
